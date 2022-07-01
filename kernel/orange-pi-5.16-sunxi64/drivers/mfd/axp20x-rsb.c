b6b9eb4a192c08be88368a345052f963c42b8985
MacLodge <shilong.native@gmail.com>
2022-07-01 10:31:22 +0800

    kernel add axp1530 pmic driver

--- kernel/orange-pi-5.16-sunxi64/drivers/mfd/axp20x-rsb.c
+++ kernel/orange-pi-5.16-sunxi64/drivers/mfd/axp20x-rsb.c
@@ -24,55 +24,56 @@
 
 static int axp20x_rsb_probe(struct sunxi_rsb_device *rdev)
 {
-	struct axp20x_dev *axp20x;
-	int ret;
+    struct axp20x_dev *axp20x;
+    int ret;
 
-	axp20x = devm_kzalloc(&rdev->dev, sizeof(*axp20x), GFP_KERNEL);
-	if (!axp20x)
-		return -ENOMEM;
+    axp20x = devm_kzalloc(&rdev->dev, sizeof(*axp20x), GFP_KERNEL);
+    if (!axp20x)
+        return -ENOMEM;
 
-	axp20x->dev = &rdev->dev;
-	axp20x->irq = rdev->irq;
-	dev_set_drvdata(&rdev->dev, axp20x);
+    axp20x->dev = &rdev->dev;
+    axp20x->irq = rdev->irq;
+    dev_set_drvdata(&rdev->dev, axp20x);
 
-	ret = axp20x_match_device(axp20x);
-	if (ret)
-		return ret;
+    ret = axp20x_match_device(axp20x);
+    if (ret)
+        return ret;
 
-	axp20x->regmap = devm_regmap_init_sunxi_rsb(rdev, axp20x->regmap_cfg);
-	if (IS_ERR(axp20x->regmap)) {
-		ret = PTR_ERR(axp20x->regmap);
-		dev_err(&rdev->dev, "regmap init failed: %d\n", ret);
-		return ret;
-	}
+    axp20x->regmap = devm_regmap_init_sunxi_rsb(rdev, axp20x->regmap_cfg);
+    if (IS_ERR(axp20x->regmap))
+    {
+        ret = PTR_ERR(axp20x->regmap);
+        dev_err(&rdev->dev, "regmap init failed: %d\n", ret);
+        return ret;
+    }
 
-	return axp20x_device_probe(axp20x);
+    return axp20x_device_probe(axp20x);
 }
 
 static void axp20x_rsb_remove(struct sunxi_rsb_device *rdev)
 {
-	struct axp20x_dev *axp20x = sunxi_rsb_device_get_drvdata(rdev);
-
-	axp20x_device_remove(axp20x);
+    struct axp20x_dev *axp20x = sunxi_rsb_device_get_drvdata(rdev);
+    axp20x_device_remove(axp20x);
 }
 
 static const struct of_device_id axp20x_rsb_of_match[] = {
-	{ .compatible = "x-powers,axp223", .data = (void *)AXP223_ID },
-	{ .compatible = "x-powers,axp803", .data = (void *)AXP803_ID },
-	{ .compatible = "x-powers,axp806", .data = (void *)AXP806_ID },
-	{ .compatible = "x-powers,axp809", .data = (void *)AXP809_ID },
-	{ .compatible = "x-powers,axp813", .data = (void *)AXP813_ID },
-	{ },
+    {.compatible = "x-powers,axp223", .data = (void *)AXP223_ID},
+    {.compatible = "x-powers,axp803", .data = (void *)AXP803_ID},
+    {.compatible = "x-powers,axp806", .data = (void *)AXP806_ID},
+    {.compatible = "x-powers,axp1530", .data = (void *)AXP1530_ID},
+    {.compatible = "x-powers,axp809", .data = (void *)AXP809_ID},
+    {.compatible = "x-powers,axp813", .data = (void *)AXP813_ID},
+    {},
 };
 MODULE_DEVICE_TABLE(of, axp20x_rsb_of_match);
 
 static struct sunxi_rsb_driver axp20x_rsb_driver = {
-	.driver = {
-		.name	= "axp20x-rsb",
-		.of_match_table	= of_match_ptr(axp20x_rsb_of_match),
-	},
-	.probe	= axp20x_rsb_probe,
-	.remove	= axp20x_rsb_remove,
+    .driver = {
+        .name = "axp20x-rsb",
+        .of_match_table = of_match_ptr(axp20x_rsb_of_match),
+    },
+    .probe = axp20x_rsb_probe,
+    .remove = axp20x_rsb_remove,
 };
 module_sunxi_rsb_driver(axp20x_rsb_driver);
 
